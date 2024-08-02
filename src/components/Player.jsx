79cab82a1dc907218003355aa1ec0fd63d7f9060
Chukwubambo <adebamboomolola31@gmail.com>
2024-08-02 21:42:31 +0100

    Display Time

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,45 +1,60 @@
-import React, { useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import { assets, songsData } from "../assets/assets";
 import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
-
-  const {audioref, seekBar, seekBg, playStatus, play, pause} = useContext(PlayerContext);
+  const {
+    track,
+    audioref,
+    seekBar,
+    seekBg,
+    playStatus,
+    play,
+    pause,
+    time,
+    previous,
+    next,
+    seekSong,
+  } = useContext(PlayerContext);
 
   useEffect(() => {
     if (audioref.current) {
-      audioref.current.src = songsData[0].file;
+      audioref.current.src = track.file;
     }
-  }, [audioref]);
-
+  }, [audioref, track]);
 
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
-       <audio ref={audioref}></audio>
+      <audio ref={audioref}></audio>
       <div className="hidden lg:flex items-center gap-4">
-        <img className="w-12" src={songsData[0].image} alt="" />
+        <img className="w-12" src={track.image} alt="" />
         <div>
-          <p>{songsData[0].name}</p>
-          <p>{songsData[0].desc.slice(0,12)}</p> 
+          <p>{track.name}</p>
+          <p>{track.desc.slice(0, 12)}</p>
         </div>
       </div>
       <div className="flex flex-col items-center gap-1 m-auto">
         <div className="flex gap-4">
           <img className="w-4 cursor-pointer" src={assets.shuffle_icon} alt="" />
-          <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
-          {playStatus
-          ?<img onClick={pause} className="w-4 cursor-pointer" src={assets.pause_icon} alt="" />
-        :<img onClick={play}  className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
-        }
-         <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
+          <img onClick={previous} className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
+          {playStatus ? (
+            <img onClick={pause} className="w-4 cursor-pointer" src={assets.pause_icon} alt="" />
+          ) : (
+            <img onClick={play} className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
+          )}
+          <img onClick={next} className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
           <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
         </div>
         <div className="flex items-center gap-5">
-          <p>1:06</p>
-          <div ref={seekBg} className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
-            <hr ref={seekBar} className="h-1 border-none w-0 bg-green-800 rounded-full"/>
+          <p>
+            {time.currentTime.minute}:{time.currentTime.second < 10 ? `0${time.currentTime.second}` : time.currentTime.second}
+          </p>
+          <div ref={seekBg} onClick={seekSong} className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
+            <hr ref={seekBar} className="h-1 border-none w-0 bg-green-800 rounded-full" />
           </div>
-          <p>3:20</p>
+          <p>
+            {time.totalTime.minute}:{time.totalTime.second < 10 ? `0${time.totalTime.second}` : time.totalTime.second}
+          </p>
         </div>
       </div>
       <div className="hidden lg:flex items-center gap-2 opacity-75">
@@ -48,14 +63,12 @@ const Player = () => {
         <img className="w-4" src={assets.queue_icon} alt="" />
         <img className="w-4" src={assets.speaker_icon} alt="" />
         <img className="w-4" src={assets.volume_icon} alt="" />
-        <div className="w-20 bg-slate-50 h-1 rounded">
+        <div className="w-20 bg-slate-50 h-1 rounded"></div>
         <img className="w-4" src={assets.mini_player_icon} alt="" />
         <img className="w-4" src={assets.zoom_icon} alt="" />
-        </div>
       </div>
     </div>
   );
 };
 
 export default Player;
-
